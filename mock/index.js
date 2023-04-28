016891b0cfbaf0be55e36909fdf8da8e4f7a2f00
Dio.Ye <diov87@outlook.com>
2023-04-28 16:21:00 +0800

    feature: Mock api server

---
+++ mock/index.js
@@ -0,0 +1,38 @@
+import http from 'node:http'
+import console from 'node:console'
+import fs from 'node:fs'
+import path from 'node:path'
+
+const host = 'localhost'
+const port = 1234
+const dirPath = path.join(process.cwd(), 'mock')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5174')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  if (req.url === '/get_map') {
+    res.setHeader('Content-Type', 'application/json')
+    fs.readFile(`${dirPath}/grid.json`, (_, data) => {
+      res.end(data)
+    })
+  }
+  else if (req.url === '/get_robot_data') {
+    res.setHeader('Content-Type', 'application/json')
+    fs.readFile(`${dirPath}/pose.json`, (_, data) => {
+      res.end(JSON.stringify({
+        robot_pose: JSON.parse(data.toString()),
+      }))
+    })
+  }
+  else {
+    res.end('Hello World')
+  }
+})
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
