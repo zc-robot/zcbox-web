d76c4af7d74ba59db58fa0723b80c4ec13e7c1ad
Dio.Ye <diov87@outlook.com>
2023-05-05 19:58:19 +0800

    feature: Use Unocss preflights config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import 'virtual:uno.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './ui/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
--- unocss.config.ts
+++ unocss.config.ts
@@ -5,6 +5,19 @@ import presetAttributify from '@unocss/preset-attributify'
 import transformerAttributifyJsx from '@unocss/transformer-attributify-jsx'
 
 export default defineConfig({
+  preflights: [{
+    getCSS: () => `
+    html, body, #root {
+      height: 100%;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+    `,
+  }],
   shortcuts: {
     'panel-container': 'bg-dark-300 flex flex-items-center justify-between',
     'panel-item': 'flex hover:bg-dark-800 h-3rem w-2.5rem color-white',
