98316400e22bcb2b8bf4c939485a702f214dbe43
Dio.Ye <diov87@outlook.com>
2023-06-07 15:59:34 +0800

    feature: Support drop upload large file

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import Monitor from './map/Monitor'
 import Breadcrumb from './Breadcrumb'
 import InfoDeck from './info/InfoDeck'
 import TaskDeck from './task/TaskDeck'
 import ControllerDeck from './controller/ControllerDeck'
+import Monitor from './map/Monitor'
+import Uploader from './uploader/Uploader'
 
 const App: React.FC = () => {
   return (
     <div flex='~ col' h-full>
       <Breadcrumb />
       <div flex='~ auto'>
+        <Uploader />
         <InfoDeck />
         <Monitor />
         <div flex='~ col' w-12rem border='l-solid 1px gray-300'>
---
+++ src/ui/uploader/Uploader.tsx
@@ -0,0 +1,30 @@
+import { DropzoneComponent } from 'react-dropzone-component'
+import '~/react-dropzone-component/styles/filepicker.css'
+import '~/dropzone/dist/dropzone.css'
+
+const Uploader: React.FC = () => {
+  const componentConfig = {
+    iconFiletypes: ['.zip'],
+    showFiletypeIcon: true,
+    postUrl: '/api/uploadFirmware',
+  }
+  const djsConfig = {
+    paramName: 'file',
+    chunking: true,
+    acceptedFiles: '.zip',
+    maxFilesize: 1024, // megabytes
+    chunkSize: 1000000, // bytes
+    addRemoveLinks: true,
+  }
+
+  return (
+    // TODO: Implement DropzoneComponent
+    <div className={'w-100 flex-self-center'}>
+      <DropzoneComponent
+        config={componentConfig}
+        djsConfig={djsConfig} />
+    </div>
+  )
+}
+
+export default Uploader
