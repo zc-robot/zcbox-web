4c8e5b1b94b2b668ab7b1dc41ef3290b51553d7a
Dio.Ye <diov87@outlook.com>
2023-07-27 14:08:25 +0800

    feature: Enable remove task

--- src/page/deployment/TaskDeck.tsx
+++ src/page/deployment/TaskDeck.tsx
@@ -11,9 +11,11 @@ interface TaskItemProps {
   executing: boolean
   onTaskSelected: (task: NavTask) => void
   onTaskRenamed: (name: string) => void
+  onDeleteClicked: () => void
 }
 
-const TaskItem: React.FC<TaskItemProps> = ({ task, enabled, executing, onTaskSelected, onTaskRenamed }) => {
+const TaskItem: React.FC<TaskItemProps> = ({ task, enabled, executing, onTaskSelected, onTaskRenamed, onDeleteClicked }) => {
+  const [showMenu, setShowMenu] = useState(false)
   const [name, setName] = useState(task.name)
 
   useEffect(() => {
@@ -23,10 +25,24 @@ const TaskItem: React.FC<TaskItemProps> = ({ task, enabled, executing, onTaskSel
   return (
     <div
       className={`flex flex-items-center pl text-3 cursor-default h-2rem ${enabled && 'font-bold'} ${executing && 'text-green'}`}
-      onClick={() => onTaskSelected(task)}>
+      onClick={(e) => {
+        e.preventDefault()
+        onTaskSelected(task)
+        if (showMenu)
+          setShowMenu(false)
+      }}
+      onContextMenu={(e) => {
+        e.preventDefault()
+
+        if (enabled)
+          setShowMenu(true)
+      }}>
       <div
         className={`i-material-symbols-check-small mr-1 ${enabled ? '' : 'invisible'}`} />
       <EditableLabel value={name} onValueChanged={setName} onValueConfirmed={onTaskRenamed} />
+      {showMenu && <div className="z-10 relative left-5 top-5 bg-white shadow-(sm blueGray)">
+        <div className="text-(sm dark-100) p-1 hover:bg-gray-200" onClick={onDeleteClicked}>删除</div>
+      </div>}
     </div>
   )
 }
@@ -35,15 +51,23 @@ const TaskDeck: React.FC = () => {
   const [showTaskList, setShowTaskList] = useState(false)
   const [repeat, setRepeat] = useState(false)
 
-  const robotStatus = useGridStore(state => state.robotInfo?.fsm)
-  const executingTaskId = useGridStore(state => state.robotInfo?.task_uid)
-  const currentProfileId = useProfileStore(state => state.currentProfileId)
-  const currentTaskId = useProfileStore(state => state.currentTaskId)
-  const setCurrentTask = useProfileStore(state => state.setCurrentTask)
-  const currentTasks = useProfileStore(state => state.currentProfileTasks())
-  const currentEnabledTask = useProfileStore(state => state.getCurrentTask())
-  const addTask = useProfileStore(state => state.appendProfileTask)
-  const updateCurrentTask = useProfileStore(state => state.updateCurrentTask)
+  const { robotStatus, executingTaskId } = useGridStore(state => ({
+    robotStatus: state.robotInfo?.fsm,
+    executingTaskId: state.robotInfo?.task_uid,
+  }))
+  const {
+    currentProfileId, currentTaskId, setCurrentTask, currentTasks,
+    currentEnabledTask, addTask, updateCurrentTask, removeProfileTask,
+  } = useProfileStore(state => ({
+    currentProfileId: state.currentProfileId,
+    currentTaskId: state.currentTaskId,
+    setCurrentTask: state.setCurrentTask,
+    currentTasks: state.currentProfileTasks(),
+    currentEnabledTask: state.getCurrentTask(),
+    addTask: state.appendProfileTask,
+    updateCurrentTask: state.updateCurrentTask,
+    removeProfileTask: state.removeProfileTask,
+  }))
 
   const toggleTask = async () => {
     if (!currentProfileId || !currentTaskId)
@@ -88,6 +112,7 @@ const TaskDeck: React.FC = () => {
               task={t}
               onTaskSelected={() => setCurrentTask(t.uid)}
               onTaskRenamed={name => updateCurrentTask({ name })}
+              onDeleteClicked={() => removeProfileTask(t.uid)}
               />
           )
         })}
